124771c9239513b174caac078cbc77b546fa06a8
betul songul <130511016+betulsongul@users.noreply.github.com>
2023-04-26 12:02:12 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,6 +2,8 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm projesidir");
         //versiyon1
+        System.out.println("Java is easy!");
+        //versiyon2
 
 
 
